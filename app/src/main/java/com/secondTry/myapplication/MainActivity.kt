25890bf15574d73b101c420b5f50fbbcfcff0dfc
Sem_Achos <gooneech7@yandex.ru>
2021-10-27 20:21:33 +0300

    buttot and flags added

---
+++ app/src/main/java/com/secondTry/myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.secondTry.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+private
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
